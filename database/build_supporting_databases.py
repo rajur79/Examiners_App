972b5f5e61f8ad0f6cb3f2237f928ec7d79fa722
rajur79 <rajur79@gmail.com>
2020-05-05 18:47:06 +0100

    Add files via upload

---
+++ database/build_supporting_databases.py
@@ -0,0 +1,15 @@
+import json
+import requests
+import re
+import sqlite3
+import os
+
+connection = sqlite3.connect("login.db")
+
+connection.execute("DROP TABLE if exists login")
+connection.execute("CREATE TABLE login(row_num int not null, email text, password text, mobile text, last_login date, last_update date)")
+
+connection.commit()
+connection.close()
+
+print("Done creating supporting tables!!")
